407b4ae301a057421225855ef23b0db6ba3b0704
radamanth <radamanth@gmail.com>
2015-11-27 15:52:52 +0100

    FIX: creation du repertoire de backup de manière automatique

--- templates/backupDailyMongo.sh
+++ templates/backupDailyMongo.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo -u {{mongodb_backup_user}} mkdir -p /home/{{mongodb_backup_user}}/mongoDB
 
 # Purge pruge directory from backup older thant {{mongodb_backup_max_days}} 
 find /home/{{mongodb_backup_user}}/mongoDB/[0-9]* -maxdepth 0  -type d  -ctime {{mongodb_backup_max_days}} | xargs rm -rf 
