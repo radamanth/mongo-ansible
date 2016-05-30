164e3a5db3488222ffd3ec4d8040d327b91dcc7f
Tony Deboschère <radamanth@gmail.com>
2016-05-30 15:35:57 +0200

    FIX : BackDailyMongo script file max days

--- templates/backupDailyMongo.sh
+++ templates/backupDailyMongo.sh
@@ -2,7 +2,8 @@
 sudo -u {{mongodb_backup_user}} mkdir -p /home/{{mongodb_backup_user}}/mongoDB
 
 # Purge pruge directory from backup older thant {{mongodb_backup_max_days}} 
-find /home/{{mongodb_backup_user}}/mongoDB/[0-9]* -maxdepth 0  -type d  -ctime {{mongodb_backup_max_days}} | xargs rm -rf 
+find /home/{{mongodb_backup_user}}/mongoDB/[0-9]* -maxdepth 0  -type d  -ctime +{{mongodb_backup_max_days}} | xargs rm -rf
+
 
 # What's the day today.
 day=$(date +"%Y%m%d")
